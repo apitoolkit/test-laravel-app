b716a62bc38f25135b5340678ff000e9915a5f59
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-06-15 22:50:52 +0200

    init test laravel app

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use APIToolkit\SDKs\PHPSDK;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\View;
+
+class AppServiceProvider extends ServiceProvider
+{
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+
+  public function register()
+  {
+  }
+
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+  }
+}
+

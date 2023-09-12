72651f0c096adda558d62bca8dbfece44fc2f18c
Anthony Alaribe <anthonyalaribe@gmail.com>
2023-09-12 19:58:25 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+Route::get('/test', function () {
+  return response()->json([
+    'message' => 'Welcome to your new application!'
+  ]);
 });
